a6796a805c317f603a6f8c6dec31b47dea5cc04a
EverDourado <evertondmcf@hotmail.com>
2023-11-09 20:38:28 -0300

    fix: conexão API efetivada - ../src/ pages/Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,35 @@
 import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 
-const apiKey = import.meta.env.KEY;
-const moviesUrl = import.meta.env.API;
+
+//const moviesURL = import.meta.env.VITE_API;
+//const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
     const [topMovies, setTopMovies] = useState([]);
 
-    const getTopRatedMovies = async (uri) => {
+    const getTopRatedMovies = async (url) => {
 
-        const res = await fetch(uri);
+        const res = await fetch(url);
         const data = await res.json();
-
         setTopMovies(data.results);
     };
 
     useEffect(() => {
-        const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
-
+        const topRatedUrl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=331bc2971b202d503c875a3c4c6a48c4';
+        console.log(topRatedUrl);
+        
         getTopRatedMovies(topRatedUrl);
+        
 
     }, []);
 
+    console.log(topMovies);
+
     return (
         <div className="container">
-            <h2 className="title">Melhores Filmes:</h2>
+            <h2 className="title">Melhores filmes:</h2>
             <div className="movies-container">
-                {topMovies.length === 0 && <p>Carregando...</p>}
                 {topMovies.length > 0 && 
                 topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
             </div>
